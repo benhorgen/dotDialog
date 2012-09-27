c66351d09f96ecaff9009ed30dc9df99b32ac783
Ben Horgen <ben@benhorgen.com>
2012-09-27 12:01:51 -0500

    Refactored the GlassButton creation into two methods to allow a click handler to be added

--- MonoTouch.Dialog-AddOn/GlassButtonSection.cs
+++ MonoTouch.Dialog-AddOn/GlassButtonSection.cs
@@ -6,25 +6,42 @@ namespace MonoTouch.Dialog.AddOn
 {
 	public static class GlassButtonExtension
 	{
-		public static Element CreateGlassButtonElement(string buttonLabelText, string uri)
+		public static Element CreateGlassButtonElement(GlassButton button)
 		{
-			float labelHeight = 44;
-			float buttonWidth = 300;
-			GlassButton button = new GlassButton(new RectangleF(0, 0, buttonWidth, labelHeight));
-			button.NormalColor = UIColor.FromRGB(0, 63, 107);
-			button.HighlightedColor = UIColor.LightGray;
-			button.SetTitleColor(UIColor.FromRGBA(255, 255, 255, 255), UIControlState.Normal);
-			button.SetTitleColor(UIColor.FromRGBA(0, 0, 0, 255), UIControlState.Highlighted);
-			button.HorizontalAlignment = UIControlContentHorizontalAlignment.Center;
-			button.SetTitle(buttonLabelText, UIControlState.Normal);
-			button.TouchUpInside += (sender, e) =>
-			{ 
-				//TODO:  Find a away to pass in an TouchUpInside delegate instead of a Uri
-			};
 			UIViewElement imageElement = new UIViewElement(null, button, true);
 			imageElement.Flags = UIViewElement.CellFlags.DisableSelection | UIViewElement.CellFlags.Transparent;				
 			return imageElement;
 		}
+
+		public static GlassButton CreateGlassButton(string buttonLabelText) 
+		{ 
+			return CreateGlassButton(buttonLabelText, DefaultColor, DefaultButtonWidth, DefaultButtonHeight);
+		}
+
+		public static GlassButton CreateGlassButton(string buttonLabelText, UIColor color) 
+		{ 
+			return CreateGlassButton(buttonLabelText, color, DefaultButtonWidth, DefaultButtonHeight);
+		}
+
+		public static GlassButton CreateGlassButton(string buttonTitle, UIColor color, float width, float height)
+		{
+			GlassButton button = new GlassButton(new RectangleF(0, 0, width, height));
+			button.NormalColor = color;
+			button.HighlightedColor = UIColor.LightGray;
+			button.SetTitleColor(UIColor.FromRGBA(255, 255, 255, 255), UIControlState.Normal);
+			button.SetTitleColor(UIColor.FromRGBA(0, 0, 0, 255), UIControlState.Highlighted);
+			button.HorizontalAlignment = UIControlContentHorizontalAlignment.Center;
+			button.SetTitle(buttonTitle, UIControlState.Normal);
+
+			//TODO:  Find a away to pass in an TouchUpInside delegate instead of a Uri
+			//button.TouchUpInside += (sender, e) => { };
+
+			return button;
+		}
+
+		public const float DefaultButtonHeight = 44;
+		public const float DefaultButtonWidth = 300;
+		public static UIColor DefaultColor = UIColor.FromRGB(0, 63, 107);
 	}
 }
 
